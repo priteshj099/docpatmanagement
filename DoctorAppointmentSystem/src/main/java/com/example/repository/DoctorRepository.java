23f19ebc2f494ec0efb2bb23e7fae745327b6631
priteshj099 <priteshj099@gmail.com>
2025-04-26 02:27:02 +0530

    Delete DoctorAppointmentSystem/src directory

--- DoctorAppointmentSystem/src/main/java/com/example/repository/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
--- DoctorAppointmentSystem/src/main/java/com/example/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
