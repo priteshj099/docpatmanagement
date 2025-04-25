81308496363ba958347d6d564facaf80569bbda2
priteshj099 <priteshj099@gmail.com>
2025-04-26 03:25:43 +0530

    Delete TECHPLEMENT-Web01-main directory

--- TECHPLEMENT-Web01-main/TechplementWeb1/README.md
+++
@@ -1,1 +0,0 @@
-# TechplementWeb1
--- TECHPLEMENT-Web01-main/src/context/DoctorContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DoctorContext = React.createContext({
-    setRegisterDetails: () => {},
-    rgDetails: []
-})
-
-export default DoctorContext
-
